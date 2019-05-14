eeae611c71c402eaadef648eac37f740059ee31d
Nolan S. Code <33040617+ncode3@users.noreply.github.com>
2019-05-14 11:59:36 -0400

    Update run_oncloud.sh

--- courses/streaming/process/sandiego/run_oncloud.sh
+++ courses/streaming/process/sandiego/run_oncloud.sh
@@ -6,9 +6,9 @@ if [ "$#" -lt 3 ]; then
    exit
 fi
 
-PROJECT=$1
+PROJECT=qwiklabs-gcp-8832a46347559c3f
 shift
-BUCKET=$1
+BUCKET=qwiklabs-gcp-8832a46347559c3f
 shift
 MAIN=com.google.cloud.training.dataanalyst.sandiego.$1
 shift
